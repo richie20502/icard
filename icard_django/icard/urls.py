d15a4637814bfbb2c8af242d8597bab61a0447d4
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-03 16:38:08 -0600

    se modifico el archivo url icard backend y genero el proyecto front react

--- icard_django/icard/urls.py
+++ icard_django/icard/urls.py
@@ -22,9 +22,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-    title="Snippets API",
+    title="iCard API",
     default_version='v1',
-    description="Test description",
+    description="descripción",
     terms_of_service="https://www.google.com/policies/terms/",
     contact=openapi.Contact(email="contact@snippets.local"),
     license=openapi.License(name="BSD License"),
