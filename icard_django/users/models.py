98c716c3e4804362bc28f984fad683e8bfdb7266
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-06 10:35:20 -0600

    se cambio username a email como autentificacion

--- icard_django/users/models.py
+++ icard_django/users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True);
+
+    USERNAME_FIELD ="email"
+    REQUIRED_FIELDS=[]
 
 # Create your models here.
