f9f4f37423d2e5711cc622b32274444f0492c0a6
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-06 16:03:10 -0600

    se hizo el override de actualizar usuario

--- icard_django/users/api/views.py
+++ icard_django/users/api/views.py
@@ -14,3 +14,11 @@ class UserApiViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         request.data['password'] = make_password(request.data['password'])
         return super().create(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        password = request.data['password']
+        if password:
+            request.data['password'] = make_password(password)
+        else:
+            request.data['password'] = request.user.password
+        return super().update(request, *args, **kwargs)
