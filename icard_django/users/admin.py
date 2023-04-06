bdfa5b3a17afb73ab254b54cc9f96155d58c65aa
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-06 10:42:17 -0600

    se agrego la app user dentro del admin

--- icard_django/users/admin.py
+++ icard_django/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
