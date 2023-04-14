5a83b8beb50c74b1c5ee604b2d08c756aa04f816
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-14 15:08:41 -0600

    se agrego el routes de jwt

--- icard_django/users/api/router.py
+++ icard_django/users/api/router.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from users.api.views import UserApiViewSet, UserView
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router_user = DefaultRouter()
 
@@ -9,5 +10,6 @@ router_user.register(
 )
 
 urlpatterns = [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/me/', UserView.as_view())
 ]
