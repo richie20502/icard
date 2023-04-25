4c147c48aec6d0f72ed4f1530558967346db4953
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-25 13:27:13 -0600

    se termino la funcion de loginApi

--- icard_fronted/src/api/user.js
+++ icard_fronted/src/api/user.js
@@ -1,8 +1,25 @@
 import {BASE_API} from '../utils/constants';
 
-export async function loginApi(formData){
+export async function loginApi(formValue){
     try {
-        const url = `${BASE_API}`
+        const url = `${BASE_API}/auth/login`
+        const params = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formValue),
+        };
+
+        const response = await fetch(url, params);
+
+        if(response.status !== 200){
+            throw new Error("Usuario o constraseña incorrectas");
+        }
+
+        const result = await response.json();
+        return result;
+        
     } catch (error) {
         throw error
     }
