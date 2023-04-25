e081a64ff893a812156329d209bcb189d6533c78
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-25 16:54:21 -0600

    recuperando sesión al recargar la pagina

--- icard_fronted/src/api/token.js
+++ icard_fronted/src/api/token.js
@@ -1,4 +1,9 @@
 import {TOKEN} from '../utils/constants';
+
 export function setToken(token) {
     localStorage.setItem(TOKEN, token);
+}
+
+export function getToken(){
+    return localStorage.getItem(TOKEN);
 }
--- icard_fronted/src/context/AuthContext.js
+++ icard_fronted/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, createContext} from 'react';
-import {setToken} from '../api/token';
+import {setToken, getToken} from '../api/token';
 import { useUser } from '../hooks';
 
 export const AuthContext = createContext({
@@ -12,6 +12,23 @@ export function AuthProvaider(props) {
     const { children } = props;
     const [auth,setAuth] = useState(undefined);
     const {getMe} = useUser();
+    useEffect(() => {
+      /** fucncvion anonima autoejecutable */
+        (async () => {
+            const token = getToken();
+            if(token){
+                const me = await getMe(token);
+                setAuth({
+                    token,
+                    me
+                });
+                console.log(me);
+            }else{
+                setAuth(null);
+            }
+        })();
+    }, []);
+    
 
     const login = async(token) =>{
         setToken(token);
@@ -24,6 +41,8 @@ export function AuthProvaider(props) {
         login,
         logout: () => console.log('logout')
     }
+
+    if(auth === undefined) return null;
 
     return(
         <AuthContext.Provider value={valueContext}>
