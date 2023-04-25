6e632d47acd35fe07523cd8bcdd05765ca9f54d4
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-25 12:56:07 -0600

    se inicio funcion de loginApi

---
+++ icard_fronted/src/api/user.js
@@ -0,0 +1,9 @@
+import {BASE_API} from '../utils/constants';
+
+export async function loginApi(formData){
+    try {
+        const url = `${BASE_API}`
+    } catch (error) {
+        throw error
+    }
+}
