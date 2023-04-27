daf6077200288e9ae0db3a63fb008f384e7449d3
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-04-26 21:47:30 -0600

    se le añadio la logica al top menu

--- icard_fronted/src/components/Admin/TopMenu/TopMenu.js
+++ icard_fronted/src/components/Admin/TopMenu/TopMenu.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import "./TopMenu.scss";
 import {Icon, Menu} from "semantic-ui-react";
+import { useAuth } from '../../../hooks';
 
 export function TopMenu() {
+    const { auth, logout } = useAuth();
+
+    const renderName = () => {
+        if(auth.me?.first_name && auth.me?.last_name) {
+            return  `${auth.me.first_name} ${auth.me.last_name}`;
+        }
+        return auth.me?.email;
+    };
+
     return (
         <Menu fixed='top' className='top-menu-admin'>
             <Menu.Item className='top-menu-admin__logo'>
@@ -11,9 +21,9 @@ export function TopMenu() {
 
             <Menu.Menu position='right'>
                 <Menu.Item>
-                    Hola, Usuario ...
+                    Hola, {renderName()} ...
                 </Menu.Item>
-                <Menu.Item>
+                <Menu.Item onClick={logout}>
                     <Icon name='sign-out'/>
                 </Menu.Item>
             </Menu.Menu>
