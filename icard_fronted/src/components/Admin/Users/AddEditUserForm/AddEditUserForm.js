f52be08491326e2faaf0a9d6a873604edc7f2655
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-05-11 22:22:21 -0600

    se genero el fromulario de usaurios

--- icard_fronted/src/components/Admin/Users/AddEditUserForm/AddEditUserForm.js
+++ icard_fronted/src/components/Admin/Users/AddEditUserForm/AddEditUserForm.js
@@ -1,25 +1,68 @@
 import React from 'react';
 import { Form, Button, Checkbox } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup'; 
 import "./AddEditUserForm";
 import "./AddEditUserForm.scss";
 
-export  function AddEditUserForm() {
+export function AddEditUserForm() {  
   return (
-    <Form className='add-edit-user-form'>
-        <Form.Input name="username" placeholder="Nombre de usuario" />
-        <Form.Input name="email" placeholder="Correro Electronico" />
-        <Form.Input name="first_name" placeholder="Nombre" />
-        <Form.Input name="last_name" placeholder="Apellidos" />
-        <Form.Input name="password" type="password" placeholder="Contraseña" />
+    <Form className='add-edit-user-form' 
+      >
+        <Form.Input 
+          name="username" 
+          placeholder="Nombre de usuario" />
+
+        <Form.Input 
+          name="email" 
+          placeholder="Correro Electronico" />
+
+        <Form.Input 
+          name="first_name" 
+          placeholder="Nombre" />
+
+        <Form.Input 
+          name="last_name" 
+          placeholder="Apellidos"/>
+
+        <Form.Input 
+          name="password" 
+          type="password" 
+          placeholder="Contraseña"/>
+
         <div className='add-edit-user-form__active'>
-            <Checkbox toggle/> Usuario activo
+            <Checkbox toggle /> Usuario activo
         </div>
 
         <div className='add-edit-user-form__staff'>
-            <Checkbox toggle/> Usuario Administrador
+            <Checkbox toggle /> Usuario Administrador
         </div>
 
         <Button type='submit' primary fluid content="Crear"/>
     </Form>
-  )
+  );
+}
+
+function initialValues(){
+  return{
+    username: "",
+    email:"",
+    first_name: "",
+    last_name: "",
+    password: "",
+    is_active: true,
+    is_staff: false,
+  }
+}
+
+function newSchame(){
+  return {
+    username: Yup.string().required(true),
+    email: Yup.string().email(true).required(true),
+    first_name: Yup.string(),
+    last_name: Yup.string(),
+    password: Yup.string().required(true),
+    is_active: Yup.bool().required(true),
+    is_staff: Yup.bool().required(true),
+  }
 }
